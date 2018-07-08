6baed3a327bf8366684af6f7849617fdfd59d5a2
BlockChainBiLaoYe <40955639+BlockChainBiLaoYe@users.noreply.github.com>
2018-07-09 00:18:02 +0800

    Create examply.py

---
+++ examply.py
@@ -0,0 +1,19 @@
+from fcoin import Fcoin
+#if python3 use fcoin3
+#from fcoin3 import Fcoin
+
+
+fcoin = Fcoin()
+
+print(fcoin.get_symbols())
+
+print(fcoin.get_currencies())
+
+fcoin.auth('key', 'secret') 
+
+print(fcoin.get_balance())
+
+#print(fcoin.buy('fteth', 0.0001, 10))
+#print(fcoin.sell('fteth', 0.002, 5))
+#print(fcoin.cancel_order('6TfBZ-eORp4_2nO5ar6zhg0gLLvuWzTTmL1OzOy9OYg='))
+#print(fcoin.get_candle('M1','fteth'))
